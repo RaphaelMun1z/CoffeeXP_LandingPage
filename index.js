5026ca53b94afe5b38195c9da9443520251cbe47
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2023-12-30 18:46:26 -0300

    Atualização: Front

--- index.js
+++ index.js
@@ -12,25 +12,88 @@ app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
+app.get('/product/:id', (req, res) => {
+    const productId = parseInt(req.params.id);
+
+    const products = [
+        {
+            id: 1,
+            title: 'Sublime Elegance Coffee',
+            description: 'Sinta o abraço acolhedor de nosso Café Especial, uma mistura meticulosamente selecionada que transcende as expectativas. Originado de grãos arábica de alta qualidade, este café oferece uma experiência sensorial única. Delicie-se com a suavidade equilibrada e notas sutis de chocolate, complementadas por uma acidez moderada. Cada gole revela uma jornada de sabores refinados, proporcionando momentos de puro prazer e indulgência. Deixe-se envolver pela riqueza aromática e pela profundidade de caráter deste café que celebra o extraordinário em cada xícara.',
+            imagePath: '/img/product_cafe_1.png',
+            category: 'coffee',
+            price: 169.90.toFixed(2),
+        },
+        {
+            id: 2,
+            title: 'Dark Harmony Espresso',
+            description: 'Descubra a intensidade única do nosso Espresso Dark Harmony. Elaborado com grãos robusta de primeira qualidade, este café oferece uma explosão de sabor encorpado e uma textura sedosa. Notas aveludadas de cacau e um toque de amêndoas criam uma experiência excepcional. Uma escolha para os amantes de cafés ousados e inesquecíveis.',
+            imagePath: `/img/product_cafe_${2}.png`,
+            category: 'coffee',
+            price: 89.50.toFixed(2),
+        },
+        {
+            id: 3,
+            title: 'Tranquil Chamomile Infusion',
+            description: 'Deleite-se com nossa Infusão de Camomila Tranquil. Uma fusão suave de flores de camomila colhidas à mão, proporcionando uma bebida relaxante e reconfortante. Cada gole é uma pausa serena, infundindo calma e bem-estar. Descubra a harmonia em cada momento com esta infusão delicadamente perfumada.',
+            imagePath: `/img/product_cafe_${3}.png`,
+            category: 'tea',
+            price: 29.90.toFixed(2),
+        },
+        {
+            id: 4,
+            title: 'Velvet Raspberry Hot Chocolate',
+            description: 'Aconchegue-se com nosso Chocolate Quente Velvet Raspberry. Uma indulgência irresistível, combinando a cremosidade do chocolate quente com a explosão de sabores de framboesa. Este deleite suave é uma celebração do prazer indulgente, perfeito para os amantes de uma experiência doce e reconfortante.',
+            imagePath: `/img/product_cafe_${4}.png`,
+            category: 'chocolate',
+            price: 39.99.toFixed(2),
+        },
+    ]
+
+    const selectedProduct = products.find(product => product.id === productId);
+
+    if (selectedProduct) {
+        console.log(`Produto de ID ${productId} foi encontrado!`);
+        res.render('product', { product: selectedProduct });
+    } else {
+        console.log(`Produto de ID ${productId} não encontrado!`);
+        res.status(404).send('Produto não encontrado');
+    }
+})
+
 app.get('/', (req, res) => {
     const products = [
         {
-            title: 'Laranjeira',
-            category: 'Frutífera',
-            body: 'A laranjeira é uma árvore de folhas perenes, conhecida por suas folhas verde-escuras e brilhantes. Suas flores brancas desabrocham em deliciosas laranjas, apreciadas por seu sabor cítrico e alto teor de vitamina C.',
-            price: 30,
+            id: 1,
+            title: 'Sublime Elegance Coffee',
+            description: 'Sinta o abraço acolhedor de nosso Café Especial, uma mistura meticulosamente selecionada que transcende as expectativas. Originado de grãos arábica de alta qualidade, este café oferece uma experiência sensorial única. Delicie-se com a suavidade equilibrada e notas sutis de chocolate, complementadas por uma acidez moderada. Cada gole revela uma jornada de sabores refinados, proporcionando momentos de puro prazer e indulgência. Deixe-se envolver pela riqueza aromática e pela profundidade de caráter deste café que celebra o extraordinário em cada xícara.'.substring(0, 90) + '...',
+            imagePath: '/img/product_cafe_1.png',
+            category: 'coffee',
+            price: 169.90.toFixed(2),
         },
         {
-            title: 'Bananeira',
-            category: 'Frutífera',
-            body: 'A bananeira é uma planta tropical de folhas largas, produzindo cachos de bananas alongadas. Rápido crescimento e rica em potássio, é uma importante fonte de alimentos em climas tropicais.',
-            price: 20,
+            id: 2,
+            title: 'Dark Harmony Espresso',
+            description: 'Descubra a intensidade única do nosso Espresso Dark Harmony. Elaborado com grãos robusta de primeira qualidade, este café oferece uma explosão de sabor encorpado e uma textura sedosa. Notas aveludadas de cacau e um toque de amêndoas criam uma experiência excepcional. Uma escolha para os amantes de cafés ousados e inesquecíveis.'.substring(0, 90) + '...',
+            imagePath: `/img/product_cafe_${2}.png`,
+            category: 'coffee',
+            price: 89.50.toFixed(2),
         },
         {
-            title: 'Cerejeira',
-            category: 'Frutífera',
-            body: 'A cerejeira é uma árvore caduca com flores rosa pálido ou brancas na primavera. Oferece cerejas coloridas, variando do vermelho ao preto, apreciadas tanto por sua beleza ornamental quanto por seu sabor doce.',
-            price: 40,
+            id: 3,
+            title: 'Tranquil Chamomile Infusion',
+            description: 'Deleite-se com nossa Infusão de Camomila Tranquil. Uma fusão suave de flores de camomila colhidas à mão, proporcionando uma bebida relaxante e reconfortante. Cada gole é uma pausa serena, infundindo calma e bem-estar. Descubra a harmonia em cada momento com esta infusão delicadamente perfumada.'.substring(0, 90) + '...',
+            imagePath: `/img/product_cafe_${3}.png`,
+            category: 'tea',
+            price: 29.90.toFixed(2),
+        },
+        {
+            id: 4,
+            title: 'Velvet Raspberry Hot Chocolate',
+            description: 'Aconchegue-se com nosso Chocolate Quente Velvet Raspberry. Uma indulgência irresistível, combinando a cremosidade do chocolate quente com a explosão de sabores de framboesa. Este deleite suave é uma celebração do prazer indulgente, perfeito para os amantes de uma experiência doce e reconfortante.'.substring(0, 90) + '...',
+            imagePath: `/img/product_cafe_${4}.png`,
+            category: 'chocolate',
+            price: 39.99.toFixed(2),
         },
     ]
 
