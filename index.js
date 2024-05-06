bd8840812416d6a599c6b053d0bf4f58e2dac9b4
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-05-05 21:42:03 -0300

    Att

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const hbs = exphbs.create({
 app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
 
-app.use(express.static('public'))
+app.use(express.static(__dirname + '../../' + '/public'))
 
 const products = [
     {
