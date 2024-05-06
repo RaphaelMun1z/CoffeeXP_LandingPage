eb4125328906dd88b0ed50f5066fd9fa0661df4d
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-05-05 21:58:04 -0300

    Att

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require("express")
-const { engine } = require("express-handlebars")
+const handlebars = require('express-handlebars')
 const path = require("path")
 
 const app = express()
 
-app.engine('handlebars', engine());
-
 app.set('view engine', 'handlebars')
-app.set('views', "./views")
+app.engine('handlebars', handlebars.engine());
 
 app.use('/public', express.static(path.join(__dirname, 'public')))
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json())
 
 const products = [
     {
