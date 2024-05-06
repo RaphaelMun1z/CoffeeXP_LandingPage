4fa20c86c8385f3457229127cdc832d8f5dd1146
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-05-05 22:03:00 -0300

    Att

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require("express")
-const handlebars = require('express-handlebars')
-const path = require("path")
+const exphbs = require("express-handlebars")
 
 const app = express()
 
+const hbs = exphbs.create({
+    partialsDir: ["views/partials"],
+})
+
+app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
-app.engine('handlebars', handlebars.engine());
 
-app.use('/public', express.static(path.join(__dirname, 'public')))
-
-app.use(express.urlencoded({extended: false}));
-app.use(express.json())
+app.use(express.static('public'))
 
 const products = [
     {
